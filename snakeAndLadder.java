8b4da7ad7a2c7f0d24339f817916d42ca2351a0d
nikhil218 <nikhil.tirpude317@gmail.com>
2021-04-28 00:34:18 +0530

    welcome to snake and ladder problem

---
+++ snakeAndLadder.java
@@ -0,0 +1,9 @@
+public class snakeAndLadder {
+   public static void main(String[] args) {
+      System.out.println("Welcome to snake and ladder problem");
+      int player_position = 0 ;
+      System.out.println( player_position);
+
+   }
+}
+
