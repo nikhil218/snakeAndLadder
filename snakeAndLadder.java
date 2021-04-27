f788b51d7510cb0ac2b1a34dd6e916c27019c6c6
nikhil218 <nikhil.tirpude317@gmail.com>
2021-04-28 00:42:12 +0530

    code to calculate the value of dice roll

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -3,7 +3,7 @@ public class snakeAndLadder {
       System.out.println("Welcome to snake and ladder problem");
       int player_position = 0 ;
       System.out.println( player_position);
-
+      int dice_value = (int)Math.floor(Math.random() * 10) % 6 + 1;
+       System.out.println( dice_value );
    }
 }
-
