0330086c267c2c7287ae53fc40d6113f88d4202c
nikhil218 <nikhil.tirpude317@gmail.com>
2021-04-28 00:45:29 +0530

    code to check possible option for player

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -1,9 +1,27 @@
 public class snakeAndLadder {
    public static void main(String[] args) {
+
       System.out.println("Welcome to snake and ladder problem");
+
       int player_position = 0 ;
-      System.out.println( player_position);
       int dice_value = (int)Math.floor(Math.random() * 10) % 6 + 1;
-       System.out.println( dice_value );
+      int player_option_check = (int)Math.floor(Math.random() * 10) % 3 ;
+
+      switch (player_option_check) {
+         case 0:
+            System.out.println("No move");
+            break;
+         case 1:
+            System.out.println("Case of Ladder");
+            player_position += dice_value;
+            System.out.println(player_position);
+            break;
+          case 2:
+            System.out.println("Case of Snake");
+            player_position -= dice_value;
+            System.out.println(player_position);
+            break;
+
+      }
    }
 }
