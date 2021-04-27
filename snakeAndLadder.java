ae4e3b73f80a4bea456f222779802a7934c8e5e0
nikhil218 <nikhil.tirpude317@gmail.com>
2021-04-28 00:49:35 +0530

    code to repeat until player reaches winning position

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -4,10 +4,12 @@ public class snakeAndLadder {
       System.out.println("Welcome to snake and ladder problem");
 
       int player_position = 0 ;
-      int dice_value = (int)Math.floor(Math.random() * 10) % 6 + 1;
-      int player_option_check = (int)Math.floor(Math.random() * 10) % 3 ;
+      int winning_position = 100 ;
 
-      switch (player_option_check) {
+      while (player_position <  winning_position) {
+         int dice_value = (int)Math.floor(Math.random() * 10) % 6 + 1;
+         int player_option_check = (int)Math.floor(Math.random() * 10) % 3 ;
+         switch (player_option_check) {
          case 0:
             System.out.println("No move");
             break;
@@ -18,10 +20,12 @@ public class snakeAndLadder {
             break;
           case 2:
             System.out.println("Case of Snake");
-            player_position -= dice_value;
+            if ( (player_position - dice_value) >= 0 ){
+               player_position -= dice_value;
+            }
             System.out.println(player_position);
             break;
-
+         }
       }
    }
 }
