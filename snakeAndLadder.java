dedd0382852982a7b604e61bf44f7c7d4eb65936
nikhil218 <nikhil.tirpude317@gmail.com>
2021-04-28 00:59:38 +0530

    code added to calculate the turns were required

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -5,10 +5,11 @@ public class snakeAndLadder {
 
       int player_position = 0 ;
       int winning_position = 100 ;
-
+      int number_of_turns = 0;
       while (player_position <  winning_position) {
          int dice_value = (int)Math.floor(Math.random() * 10) % 6 + 1;
          int player_option_check = (int)Math.floor(Math.random() * 10) % 3 ;
+         number_of_turns ++;
          switch (player_option_check) {
          case 0:
             System.out.println("No move");
@@ -30,5 +31,6 @@ public class snakeAndLadder {
             break;
          }
       }
+      System.out.println("number_of_turns " + number_of_turns);
    }
 }
